a73de32d504fe594be9d491f51735eeea2f4bd4a
Ry Jones <ry@linux.com>
2020-08-22 12:19:31 -0700

    Add my feeds, parsed
    Signed-off-by: Ry Jones <ry@linux.com>

---
+++ m.py
@@ -0,0 +1,16 @@
+from ics import *
+import urllib.request
+from datetime import datetime, timedelta
+
+url = 'file:///Users/ry/W/giocal/ry-feed.ics'
+
+with urllib.request.urlopen(url) as response:
+   ics_string = response.read()
+
+window_start = datetime.now(timezone.utc)
+window_end = window_start + timedelta(days=92)
+
+events = get_events_from_ics(ics_string, window_start, window_end)
+
+for e in events:
+    print('{} - {}'.format(e['startdt'], e['summary']))
